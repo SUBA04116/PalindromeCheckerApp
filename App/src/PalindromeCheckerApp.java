b1b6f1879d048c648dbc1f8a7d6df00865652f39
SUBA04116 <sa6931@srmist.edu.in>
2026-02-24 14:39:03 +0530

    added UC2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,34 +1,38 @@
-
 /**
- MAIN CLASS UseCaselPalindrome App
+ MAIN CLASS UseCaseiPalindromeApp
  Use Case 1: Application Entry & Welcome Message
  Description:
- This class represents the entry point of the
- Palindrome Checker Management System。
+ This class represents the entry point of the * Palindrome Checker Management System.
  At this stage, the application:
- - Starts execution from the main() method
- Displays a welcome message
- - Shows application version
+ Starts execution from the main() method
+ * - Displays a welcome message
+ Shows application version
  No palindrome logic is implemented yet.
  The goal is to establish a clear startup flow.
- @author Developer
- @version 1.0 **/
-
-
+ @author Subashree
+ @version 1.0
+ */
+import java .util.Scanner;
 public class PalindromeCheckerApp {
-
     /**
-     Application entry point..
+     Application entry point.
      This is the first method executed by the JVM
      when the program starts.
-     @param args Command-line arguments
-     public static void main(String[] args) {...} **/
-
-    public static void main(String[] args) {
-        System.out.println("welcome to the palindrome checker management system");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized Successfully.");
-
-
+     @param args Command-Line arguments
+     */
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Input Text:");
+        String input = sc.next();
+        boolean ispalindrome=true;
+        int n = input.length();
+        for(int i=0;i<n/2;i++){
+            if(input.charAt(i)!=input.charAt(n-1-i)){
+                ispalindrome=false;
+                break;
+            }
+        }
+        System.out.println("is it a palindrome?"+ispalindrome);
     }
 }
